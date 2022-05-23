f721e5ade620bd81cbbf60fcd94dca9ebf17ce86
KredoF <72181870+kredoF@users.noreply.github.com>
2022-05-22 23:30:22 -0500

    Delete Discord-bot directory

--- Discord-bot/commands/messages/util/ping.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: "ping",
-    run: async (client, message, args) => {
-        message.channel.send("Pong!")
-    }
-}
--- Discord-bot/events/ready.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: "ready",
-    execute: async (client) => {
-        client.logs.info("Conexion Completa", "✅ El bot se ha conectado con exito!")
-    }
-}
